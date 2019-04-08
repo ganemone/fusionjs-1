84deefb164ed9e92648be6d8cee88d71da408a74
Leo Horie <lhorie@uber.com>
2019-04-08 10:07:58 -0700

    fix script

--- scripts/fix-symlinks.js
+++ scripts/fix-symlinks.js
@@ -2,14 +2,14 @@ const {symlinkSync: link} = require('fs');
 
 try {
   link(
-    `${__dirname}/fusion-cli/bin/cli.js`,
-    `${__dirname}/create-fusion-app/node_modules/.bin/fusion`
+    `${__dirname}/../fusion-cli/bin/cli.js`,
+    `${__dirname}/../create-fusion-app/node_modules/.bin/fusion`
   );
 } catch (e) {}
 
 try {
   link(
-    `${__dirname}/fusion-cli/bin/cli.js`,
-    `${__dirname}/fusion-apollo-universal-client/node_modules/.bin/fusion`
+    `${__dirname}/../fusion-cli/bin/cli.js`,
+    `${__dirname}/../fusion-apollo-universal-client/node_modules/.bin/fusion`
   );
 } catch (e) {}
